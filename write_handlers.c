a077be4b411ccbe702c8007c02b5326c5d4e3c3e
SurafelFikadu <surafelfikadu05@gmail.com>
2023-01-24 18:33:26 -0800

    group work

--- write_handlers.c
+++ write_handlers.c
@@ -35,7 +35,7 @@ int handle_write_char(char c, char buffer[],
 
 		if (flags & F_MINUS)
 			return (write(1, &buffer[0], 1) +
-					write(1, &buffer [BUFF_SIZE - i - 1], width - 1));
+					write(1, &buffer[BUFF_SIZE - i - 1], width - 1));
 
 		else
 			return (write(1, &buffer[BUFF_SIZE - i - 1], width - 1) +
